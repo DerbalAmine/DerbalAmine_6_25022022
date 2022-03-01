b12e84af282561562772f048726aae78106fea2e
Amine Derbal <muhammadderbal@outlook.com>
2022-03-01 22:56:58 +0100

    debut du projet

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+}); // Ce middleware qui permet d'acceder a notre API depuis n'importe quelle origine donc permet de ressoudre les erreurs de Cors (Cross Origin Resource Sharing)
+
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next(); // permet de renvoyer vers le prochain middleware
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue bravo !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
+
+module.exports = app;
