06e787153eecd6a251c3b78ae28331faa076669c
Amine Derbal <muhammadderbal@outlook.com>
2022-03-04 06:08:05 +0100

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const app = express();
 
+const sauce = require("./models/sauce");
+
+mongoose
+  .connect(
+    "mongodb+srv://Amine:Projet@6@cluster0.owdbk.mongodb.net/Amine?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//Middleware gérant les problèmes de CORS (Cross Origin Resource Sharing)
 app.use((req, res, next) => {
+  //Accepte les requêtes depuis n'importe quelle origine
   res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
-}); // Ce middleware qui permet d'acceder a notre API depuis n'importe quelle origine donc permet de ressoudre les erreurs de Cors (Cross Origin Resource Sharing)
-
-
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next(); // permet de renvoyer vers le prochain middleware
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue bravo !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
 });
 
 module.exports = app;
---
+++ models/sauce.js
@@ -0,0 +1,54 @@
+//Appel de mongoose pour utiliser plus loin sa fonction Schema
+const mongoose = require("mongoose");
+
+//Création d'un Schema (model) pour les sauces
+const sauceSchema = mongoose.Schema({
+  //Le userId utilisé lors de la création de la sauce, pour que seul son propriétaire puisse la modifier
+  userId: {
+    type: String,
+    required: true,
+  },
+  //Le nom de la sauce
+  name: {
+    type: String,
+    required: true,
+  },
+  //Le fabriquant de la sauce
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  //Une description de la sauce
+  description: {
+    type: String,
+    required: true,
+  },
+  //Le principal ingrédient épicé de la sauce
+  mainPepper: {
+    type: String,
+    required: true,
+  },
+  //L'URL menant à l'image de la sauce
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  //Le nombre de likes que la sauce a reçu
+  likes: {
+    type: Number,
+  },
+  //Le nombre de dislikes que la sauce a reçu
+  dislikes: {
+    type: Number,
+  },
+  //Un tableau contenant les userId des utilisateurs ayant liké la sauce
+  usersLiked: {
+    type: [String],
+  },
+  //Un tableau contenant les userId des utilisateurs ayant disliké la sauce
+  usersDisliked: {
+    type: [String],
+  },
+});
+
+module.exports = mongoose.model("sauce", sauceSchema);
